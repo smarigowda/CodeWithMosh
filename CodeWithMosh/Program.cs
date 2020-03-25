edc481c62579ad88492c10e6fbb55ab82e29fb94
Santosh Marigowda <santosharakere@gmail.com>
2020-03-25 19:27:09 +0000

    catching exception

--- CodeWithMosh/Program.cs
+++ CodeWithMosh/Program.cs
@@ -1,24 +1,30 @@
 ﻿using System;
 
-namespace CodeWithMosh
+namespace TypeConversion
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World !");
-            var number = 200;
-            var count = 10;
-            var letter = 'A';
-            var firstName = "Santosh";
-            var totalPrice = 22.5f;
-            var isWorking = false;
-            Console.WriteLine(number);
-            Console.WriteLine(count);
-            Console.WriteLine(totalPrice);
-            Console.WriteLine(letter);
-            Console.WriteLine(firstName);
-            Console.WriteLine(isWorking);
+            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
+            const float PI = 3.14f;
+            // PI = 3.14
+            int i = 100;
+            byte b = (byte)i; // compatible
+            string s = "Santosh";
+            //int parsedToInt = int.Parse(s);
+            //int i32 = Convert.ToInt32(s);
+            
+            try
+            {
+                var number = "1234";
+                byte b2 = Convert.ToByte(number);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Number could not be converted to byte...");
+            }
+
         }
     }
 }
