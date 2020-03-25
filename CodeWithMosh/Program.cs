9e5bb59089b115dc7d863b1451d50688a4048ae2
Santosh Marigowda <santosharakere@gmail.com>
2020-03-25 18:46:02 +0000

    variables

---
+++ CodeWithMosh/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace CodeWithMosh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World !");
+            byte number = 200;
+            int count = 10;
+            char letter = 'A';
+            string firstName = "Santosh";
+            float totalPrice = 22.5f;
+            bool isWorking = false;
+            Console.WriteLine(number);
+            Console.WriteLine(count);
+            Console.WriteLine(totalPrice);
+            Console.WriteLine(letter);
+            Console.WriteLine(firstName);
+            Console.WriteLine(isWorking);
+        }
+    }
+}
