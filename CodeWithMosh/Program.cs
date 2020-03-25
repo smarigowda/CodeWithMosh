ba3236d373d251465d172b67c43498bc499c8ee7
Santosh Marigowda <santosharakere@gmail.com>
2020-03-25 19:30:32 +0000

    compatible conversion

--- CodeWithMosh/Program.cs
+++ CodeWithMosh/Program.cs
@@ -5,24 +5,16 @@ namespace TypeConversion
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("{0} {1}", byte.MinValue, byte.MaxValue);
-            const float PI = 3.14f;
-            // PI = 3.14
-            int i = 100;
-            byte b = (byte)i; // compatible
-            string s = "Santosh";
-            //int parsedToInt = int.Parse(s);
-            //int i32 = Convert.ToInt32(s);
-            
+        {            
             try
             {
-                var number = "1234";
-                byte b2 = Convert.ToByte(number);
+                var str = "True";
+                bool b = Convert.ToBoolean(str);
+                Console.WriteLine(b);
             }
             catch (Exception ex)
             {
-                Console.WriteLine("Number could not be converted to byte...");
+                Console.WriteLine("String could not be converted to boolean...");
             }
 
         }
