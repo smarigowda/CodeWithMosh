b8546a857137e071b0de6625c733490192bd10e3
Santosh Marigowda <santosharakere@gmail.com>
2020-03-26 20:58:10 +0000

    number guessing game

--- CodeWithMosh/Program.cs
+++ CodeWithMosh/Program.cs
@@ -3,23 +3,41 @@ using CodeWithMosh;
 using CodeWithMosh.Math;
 using CodeWithMosh.Enums;
 
-namespace ControlFlow__Ex3_Factorial
+namespace ControlFlow__Ex3_Guess_A_Number
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter a number: ");
-            var input = Console.ReadLine();
-            var inputNum = Convert.ToInt32(input);
-            int factorial = 1;
+            Console.WriteLine("Guess a number 1 - 10, you have 4 chances");
+            var random = new Random();
+            int randomNum = random.Next(1, 4);
+            Console.WriteLine($"Random number choosen = {randomNum}");
+            bool found = false;
             int i = 1;
-            while(i <= inputNum)
+            while( i <= 4)
             {
-                factorial = factorial * i;
+                Console.WriteLine($"Attempt {i} :");
+                var input = Console.ReadLine();
+                var inputNumber = Convert.ToInt32(input);
+
+                if(inputNumber == randomNum)
+                {
+                    found = true;
+                    break;
+                }
                 i++;
             }
-            Console.WriteLine($"Factorial of {inputNum} is {factorial}");
+
+            if(found == true)
+            {
+                Console.WriteLine("Cogratulations ! You Win.");
+            } else
+            {
+                Console.WriteLine("Sorry, you lost :-(");
+            }
+
+            Console.WriteLine($"Random number choosen = {randomNum}");
 
         }
     }
