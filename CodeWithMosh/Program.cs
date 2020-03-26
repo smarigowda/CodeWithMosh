c8697d7992ab80662bb505f34864d815ab074288
Santosh Marigowda <santosharakere@gmail.com>
2020-03-26 17:01:16 +0000

    while loop

--- CodeWithMosh/Program.cs
+++ CodeWithMosh/Program.cs
@@ -3,29 +3,17 @@ using CodeWithMosh;
 using CodeWithMosh.Math;
 using CodeWithMosh.Enums;
 
-namespace ControlFlow_For_Loop
+namespace While_Loop
 {
     class Program
     {
         static void Main(string[] args)
         {
-            var name = "Santosh Marigowda";
-
-            for(var i = 0; i < name.Length; i++)
+            var i = 0;
+            while( i < 10)
             {
-                Console.WriteLine(name[i]);
-            }
-
-            foreach( var letter in name)
-            {
-                Console.WriteLine(letter);
-            }
-
-            var numbers = new int [] { 1, 2, 3, 4,};
-
-            foreach(int number in numbers)
-            {
-                Console.WriteLine(number);
+                Console.WriteLine(i);
+                i++;
             }
         }
     }
