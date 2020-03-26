4acbb125d4fff8cb55167342b9c76535d62d9225
Santosh Marigowda <santosharakere@gmail.com>
2020-03-26 20:59:28 +0000

    number guessing game

--- CodeWithMosh/Program.cs
+++ CodeWithMosh/Program.cs
@@ -12,7 +12,7 @@ namespace ControlFlow__Ex3_Guess_A_Number
             Console.WriteLine("Guess a number 1 - 10, you have 4 chances");
             var random = new Random();
             int randomNum = random.Next(1, 4);
-            Console.WriteLine($"Random number choosen = {randomNum}");
+            //Console.WriteLine($"Random number choosen = {randomNum}");
             bool found = false;
             int i = 1;
             while( i <= 4)
