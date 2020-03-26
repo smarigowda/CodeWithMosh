63959e9c9370d5957f85c99a9849efba4736ca46
Santosh Marigowda <santosharakere@gmail.com>
2020-03-26 21:06:30 +0000

    string split

--- CodeWithMosh/Program.cs
+++ CodeWithMosh/Program.cs
@@ -3,42 +3,22 @@ using CodeWithMosh;
 using CodeWithMosh.Math;
 using CodeWithMosh.Enums;
 
-namespace ControlFlow__Ex3_Guess_A_Number
+namespace ControlFlow__String_Split
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Guess a number 1 - 10, you have 4 chances");
-            var random = new Random();
-            int randomNum = random.Next(1, 4);
-            //Console.WriteLine($"Random number choosen = {randomNum}");
-            bool found = false;
-            int i = 1;
-            while( i <= 4)
+            var output = "1,20,3,4,5".Split(',');
+            var max = 0;
+            foreach(var letter in output)
             {
-                Console.WriteLine($"Attempt {i} :");
-                var input = Console.ReadLine();
-                var inputNumber = Convert.ToInt32(input);
-
-                if(inputNumber == randomNum)
+                if(max < Convert.ToInt32(letter))
                 {
-                    found = true;
-                    break;
+                    max = Convert.ToInt32(letter);
                 }
-                i++;
             }
-
-            if(found == true)
-            {
-                Console.WriteLine("Cogratulations ! You Win.");
-            } else
-            {
-                Console.WriteLine("Sorry, you lost :-(");
-            }
-
-            Console.WriteLine($"Random number choosen = {randomNum}");
-
+            Console.WriteLine(max);
         }
     }
 }
