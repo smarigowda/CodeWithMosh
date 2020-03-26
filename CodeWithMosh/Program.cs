e3796c842cbcb2bc0ffe9942cce01419eefc9a17
Santosh Marigowda <santosharakere@gmail.com>
2020-03-26 16:37:27 +0000

    for loop

--- CodeWithMosh/Program.cs
+++ CodeWithMosh/Program.cs
@@ -3,35 +3,18 @@ using CodeWithMosh;
 using CodeWithMosh.Math;
 using CodeWithMosh.Enums;
 
-namespace ControlFlow_Ex_4
+namespace ControlFlow_For_Loop
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter the speed limit in km /hr: ");
-            var speedlimit = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Enter the speed of a Car: ");
-            var carspeed = Convert.ToInt32(Console.ReadLine());
-
-            if(carspeed <= speedlimit)
+            for(int i = 0; i<10; i++)
             {
-                Console.WriteLine("Thanks ! Your car speed is below the speed limit.");
-            }
-
-
-            var demeritPoints = 0;
-
-            if(carspeed > speedlimit)
-            {
-                demeritPoints = (carspeed - speedlimit) / 5;
-                Console.WriteLine($"Your demerit points = {demeritPoints}");
-            }
-
-            if(demeritPoints > 12)
-            {
-                Console.WriteLine("You have crossed the max demerit points. Your license is cancelled !!!");
+                if(i%2 == 0)
+                {
+                    Console.WriteLine(i);
+                }
             }
         }
     }
