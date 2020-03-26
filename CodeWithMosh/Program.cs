06393b73cf591867c97b8aeed6c053bae999dfc4
Santosh Marigowda <santosharakere@gmail.com>
2020-03-26 07:05:41 +0000

    Control flow exercise 2

--- CodeWithMosh/Program.cs
+++ CodeWithMosh/Program.cs
@@ -3,22 +3,20 @@ using CodeWithMosh;
 using CodeWithMosh.Math;
 using CodeWithMosh.Enums;
 
-namespace ControlFlow_Ex_1
+namespace ControlFlow_Ex_2
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter a number 0 - 12: ");
-            var input = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter firsr number: ");
+            var first = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            var second = Convert.ToInt32(Console.ReadLine());
 
-            if(input >=0 && input <= 12) 
-            {
-                Console.WriteLine($"Your input is valid, well done!");
-            } else
-            {
-                Console.WriteLine($"Your input is not valid !!!");
-            }
+            var maxnum = first > second ? first : second;
+
+            Console.WriteLine($"Max Num = {maxnum}");
 
         }
     }
