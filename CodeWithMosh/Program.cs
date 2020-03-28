6f6a5d086c46398154d59efa3b7eec2ff8ac0441
Santosh Marigowda <santosharakere@gmail.com>
2020-03-28 10:45:04 +0000

    A simple class example

--- CodeWithMosh/Program.cs
+++ CodeWithMosh/Program.cs
@@ -4,36 +4,24 @@ using CodeWithMosh.Math;
 using CodeWithMosh.Enums;
 using System.Collections.Generic;
 
-namespace DateTime_Demo
+namespace Class_Demo
 {
+    class Person
+    {
+        public string Name;
+
+        public void Introduce(string name)
+        {
+            Console.WriteLine($"Hello {name}, I am {Name}");
+        }
+    }
     class Program
     {
         static void Main(string[] args)
         {
-            var date = new DateTime(2020, 1, 1);
-            Console.WriteLine(date);
-
-            var now = DateTime.Now;
-            Console.WriteLine("now =" + now);
-            var today = DateTime.Today;
-            Console.WriteLine("Today = " + today);
-
-            Console.WriteLine("Hour = " + now.Hour);
-            Console.WriteLine("Minute = " + now.Minute);
-
-            var tomorrow = now.AddDays(1);
-            var yesterday = now.AddDays(-1);
-
-            Console.WriteLine("Tomorrow = " + tomorrow);
-            Console.WriteLine("Yesterday = " + yesterday);
-
-            Console.WriteLine(tomorrow.ToLongDateString());
-            Console.WriteLine(tomorrow.ToShortDateString());
-
-            Console.WriteLine(yesterday.ToLongTimeString());
-            Console.WriteLine(yesterday.ToShortTimeString());
-
-            Console.WriteLine(today.ToString("dd-MMMM-yyy"));
+            var person = new Person();
+            person.Name = "Santosh";
+            person.Introduce("Roopa");
         }
     }
 }
